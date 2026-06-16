c3cc34f159c6e5cfc745a0d24ca9e06c90d4d31d
opilane <opilane@HP-CZC2349HV0>
2026-06-16 15:15:41 +0300

    pages

--- CityExplorer/App.xaml.cs
+++ CityExplorer/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using CityExplorer.Views;
 
 namespace CityExplorer
 {
@@ -7,6 +8,8 @@ namespace CityExplorer
         public App()
         {
             InitializeComponent();
+
+            MainPage = new MainTabbedPage();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
