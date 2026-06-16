5369e23ae06e1deb688f3a11914d3b4d3b2ea095
opilane <opilane@HP-CZC2349HV0>
2026-06-16 15:03:19 +0300

    paegs

---
+++ CityExplorer/Views/FavoritesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CityExplorer.Views;
+
+public partial class FavoritesPage : ContentPage
+{
+	public FavoritesPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CityExplorer/Views/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CityExplorer.Views;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
